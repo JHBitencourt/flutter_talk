89fa6217d1ad7d6e2678ee4c3c6232b28889281d
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2018-12-16 14:33:26 -0200

    - A classe PeopleApi vai ser responsável por fazer a requisição e download do json; - O json está localizado em json/pessoas.json e será utilizado a URL do github para simular acesso a uma API;

---
+++ lib/people_api.dart
@@ -0,0 +1,9 @@
+
+class PeopleApi {
+
+  static final String URL = "https://raw.githubusercontent.com/JHBitencourt/flutter_talk/master/lib/json/pessoas.json";
+
+  void loadJsonFromApi() {
+  }
+
+}
