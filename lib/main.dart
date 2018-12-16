c77d78d87851d83b457d500f659cbf754be05c58
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2018-12-16 13:55:18 -0200

    - Vamos criar o primeiro Widget e extrair o Text para ele; - Não precisamos manter o estado do Widget, por isso MyApp é Stateless; - Todo widget implementa o build, muito importante para o ciclo de vida dos mesmos; - MaterialApp possui diversas implementações e facilidades para criação do App, como o title da aplicação, facilita a definição de temas, routes, etc. - MaterialApp já define a direção como LTR, não precisando mais definir no Text;

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(Center(
-        child: Text("Hello Flutter!", textDirection: TextDirection.ltr)));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Talk',
+      home: Center(child:Text("Hello Flutter!")),
+    );
+  }
+}
