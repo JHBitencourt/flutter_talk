1ee7d68103ad92ca7952a9d0f40df9f38eee59fb
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2018-12-16 14:40:21 -0200

    - Para fazer a requisição HTTP GET, usamos o package http do Dart; - Adicionamos a dependência e versão no pubspec, que é responsável pelo download dos pacotes; - Dart é single thread, Future e async permitem a execução assíncrona de alguma operação que demande tempo; - Ao encontrar a palavra await, o dart retorna o método com o Future, liberando a thread principal; - O package convert permite fazer o decode do json e transformar em uma lista de Maps;

--- lib/people_api.dart
+++ lib/people_api.dart
@@ -1,9 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class PeopleApi {
 
   static final String URL = "https://raw.githubusercontent.com/JHBitencourt/flutter_talk/master/lib/json/pessoas.json";
 
   void loadJsonFromApi() {
+  }
+
+  Future<dynamic> _get() async {
+    final response = await http.get(URL);
+    return json.decode(response.body);
   }
 
 }
