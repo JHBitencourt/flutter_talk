6aab9141fb143eaa696327d7596753c55a15e7a2
Julio Henrique Bitencourt <julio.henrique.b@gmail.com>
2018-12-16 14:51:52 -0200

    - loadJsonFromApi também deve ser async, dessa vez retornando um Future de List de Person; - Usamos o construtor de Person para transformar o Map em Person;

--- lib/people_api.dart
+++ lib/people_api.dart
@@ -1,12 +1,18 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:flutter_talk/person.dart';
 
 class PeopleApi {
 
   static final String URL = "https://raw.githubusercontent.com/JHBitencourt/flutter_talk/master/lib/json/pessoas.json";
 
-  void loadJsonFromApi() {
+  Future<List<Person>> loadJsonFromApi() async {
+    List<dynamic> responseJon = await _get();
+
+    List<Person> people = new List();
+    responseJon.forEach((map) => people.add(Person.fromJson(map)));
+    return people;
   }
 
   Future<dynamic> _get() async {
